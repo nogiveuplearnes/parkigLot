784479d30d1c992e656ea3098b1a85cd2dffe6e1
Satabdi Lal <satabdilal@jlayak-personal.local>
2020-06-20 16:51:21 -0700

    log added

--- src/main/java/com/self/parkinglot/controller/ParkingController.java
+++ src/main/java/com/self/parkinglot/controller/ParkingController.java
@@ -5,6 +5,8 @@ import com.self.parkinglot.domain.EnterIn;
 import com.self.parkinglot.domain.EnterOut;
 import com.self.parkinglot.dao.ParkingRepo;
 import com.self.parkinglot.service.ParkingLotService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,8 @@ import java.util.List;
 
 @RestController
 public class ParkingController {
+
+    private static Logger logger= LoggerFactory.getLogger(ParkingController.class);
     @Autowired
     ParkingRepo repo;
 
@@ -27,6 +31,7 @@ public class ParkingController {
     @PostMapping("/parking")
     @ResponseBody
     public EnterIn saveParking(EnterIn eIn) {
+        logger.info("Entered parking controller");
         repo.save(eIn);
         return eIn;
     }
